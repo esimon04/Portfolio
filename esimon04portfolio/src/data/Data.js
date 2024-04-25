7e0e7687af60cddfa697889d54c23c1a3b0a51cb
esimon04 <101423055+esimon04@users.noreply.github.com>
2024-04-24 21:08:12 -0400

    added games

---
+++ esimon04portfolio/src/data/Data.js
@@ -0,0 +1,40 @@
+// src/data.js
+import rythmImg from "./rythmRunner.png"
+import ballsImg from "./balls.PNG"
+import wizardImg from "./Wizard.PNG"
+import cubeImg from "./CubeGame.PNG"
+
+export const projects = [
+  {
+    title: "Rhythm Runner",
+    subtitle: "Made with Unity",
+    description:
+      "This was a basic rythm game I have been working on.",
+    image: rythmImg,
+    link: "https://esimon04.itch.io/rhythm-runner",
+  },
+  {
+    title: "Falling Cubes",
+    subtitle: "Made with Unity",
+    description:
+      "Puzzle game where the user controls a block that rotates on it's edges. ",
+    image: cubeImg,
+    link: "https://esimon04.itch.io/falling-cubes",
+  },
+  {
+    title: "Wizarding Wizard",
+    subtitle: "Made with Unity",
+    description:
+      "Top down game where the player controls a wizard. This was meant to be a learning project.",
+    image: wizardImg,
+    link: "https://esimon04.itch.io/wizarding-wizard",
+  },
+  {
+    title: "Just Two Balls",
+    subtitle: "Made with Unity",
+    description:
+      "Extremely simplistic multiplayer game to understand networking through unity.",
+    image: ballsImg,
+    link: "https://esimon04.itch.io/2d-topdown-shooter",
+  }
+];
